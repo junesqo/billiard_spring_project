5889a975a0e8f682bc7c068d4eb30e97d898e66f
Nurel Sovetbekov <62104475+junesqo@users.noreply.github.com>
2024-03-17 18:08:42 +0600

    Initial commit

---
+++ src/main/java/kg/junesqo/billiard/BilliardApplication.java
@@ -0,0 +1,13 @@
+package kg.junesqo.billiard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilliardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilliardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/junesqo/billiard/BilliardApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.junesqo.billiard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilliardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
