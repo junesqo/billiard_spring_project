9c34f63a0cbf41919945c48ef88821547ce987bd
Nurel Sovetbekov <62104475+junesqo@users.noreply.github.com>
2024-03-17 18:22:10 +0600

    JPA Entities added

---
+++ src/main/java/kg/junesqo/billiard/domain/Product.java
@@ -0,0 +1,40 @@
+package kg.junesqo.billiard.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String name;
+    private Integer price;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+
