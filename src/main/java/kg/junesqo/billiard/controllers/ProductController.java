3d4c5be93f8ab0cb874ec0996199a7dc7949a48c
Nurel Sovetbekov <62104475+junesqo@users.noreply.github.com>
2024-03-24 15:18:37 +0600

    Controller added

---
+++ src/main/java/kg/junesqo/billiard/controllers/ProductController.java
@@ -0,0 +1,26 @@
+package kg.junesqo.billiard.controllers;
+
+import kg.junesqo.billiard.services.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @RequestMapping("/products")
+    public String getProducts(Model model) {
+
+        model.addAttribute("products", productService.findAll());
+
+        return "products";
+
+    }
+
+}
